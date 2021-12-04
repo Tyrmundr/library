76418e83ae2f25453513ccd6def0f2a839ad3f72
Tyrmundr <taki.96061@gmail.com>
2021-12-04 15:53:29 +0100

    Finished JS. Will re-do since wasn't 100% individual work. needed help

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ const btnAdd = document.getElementById("btn-add")
 
 //Book constructor
 function Book(title, author, pages, read) {
-    this.title = title; // form.title.value;
-    this.author = author; //form.author.value;
-    this.pages = pages; //form.pages.value;
-    this.read = read; //form.read.value;
+    this.title = form.title.value;
+    this.author = form.author.value;
+    this.pages = form.pages.value;
+    this.read = form.read.value;
 }
 
 //Event Listeners:
@@ -24,13 +24,22 @@ function Book(title, author, pages, read) {
 btnNew.addEventListener("click", () => popupDisplay.style.display = "block");
 
 // Add new book to library on click
-btnAdd.addEventListener("click", addToLibrary);
+btnAdd.addEventListener("click", checkInput);
 
 //Functions
 
 //Adding book to library
 let myLibrary = [];
 let newBook;
+
+function checkInput() {
+    event.preventDefault();
+    if(form.title.value && form.author.value && form.pages.value) {
+        addToLibrary();
+    } else {
+        alert(`You have not given an answer everywhere!`)
+    }
+}
 
 function addToLibrary() {
     event.preventDefault();
@@ -39,15 +48,68 @@ function addToLibrary() {
     newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 
+    render();
     form.reset();
 }
 
 //Render function - for displaying books
 function render() {
-    const display = document.getElementById("library")
-    const books = document.querySelectorAll(".book")
+    const display = document.getElementById("library");
+    const books = document.querySelectorAll(".book");
+    books.forEach(book => display.removeChild(book));
 
     for(let i = 0; i < myLibrary.length; i++) {
         createBook(myLibrary[i])
     }
+}
+
+//Creating the book - for displaying it in browser
+function createBook(item) {
+    //Caching DOM elements to add:
+    //Referencing the Library div - it includes all different books we'll add
+    const library = document.getElementById("library");
+
+    //Div which'll contain all the details
+    const bookDiv = document.createElement("div");
+    //setting attribute by which it can be removed both by rendering and remove btn
+    bookDiv.classList.add("book");
+    bookDiv.setAttribute("id", myLibrary.indexOf(item));
+
+    //Div for each line of text-info
+    const paraTitle = document.createElement("div");
+    const paraAuthor = document.createElement("div");
+    const paraPages = document.createElement("div");
+
+    //Button for read or not property
+    const paraRead = document.createElement("button");
+
+    //Button to remove a book
+    const btnRemove = document.createElement("button");
+
+    //Creating and assigning elements
+    paraTitle.innerText = item.title;
+    bookDiv.appendChild(paraTitle);
+
+    paraAuthor.innerText = item.author;
+    bookDiv.appendChild(paraAuthor);
+
+    paraPages.innerText = item.pages;
+    bookDiv.appendChild(paraPages);
+
+    bookDiv.appendChild(paraRead);
+    if(item.read === false) {
+        paraRead.innerText = "Unread";
+    } else {
+        paraRead.innerText = "Read";
+    }
+
+    btnRemove.innerText = "Remove";
+    bookDiv.appendChild(btnRemove);
+
+    library.appendChild(bookDiv);
+
+    btnRemove.addEventListener("click", () => {
+        myLibrary.splice(myLibrary.indexOf(item), 1);
+        render();
+    })
 }
